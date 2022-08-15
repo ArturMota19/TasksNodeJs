48637c4a7b6911a39bd4941cb0d53d9465fb73fa
Artur.Mota <arturrodriguesmota@hotmail.com>
2022-08-15 20:17:38 -0300

    Puxando dados das rotas e usando Each para mostrar na home

--- 1_ Task_Sprint/index.js
+++ 1_ Task_Sprint/index.js
@@ -1,10 +1,47 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
+const bodyParser = require('body-parser')
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
+
+const todoList = []
+
+app.get('/:id', (req, res) => {
+    const id = req.params
+    return res.json(todoList[id])
+})
+
 app.get('/', (req, res) => {
-    res.render('home')
+    res.render('home', { todoList: todoList })
+})
+
+// Criando nova tarefa
+app.post('/', urlencodedParser, (req, res) => {
+    const nome = req.body.nome
+    todoList.push(nome)
+    res.status(201).redirect('/');
+})
+
+// Atualizando tarefa
+app.put('/:id', (req, res) => {
+    const id = req.params
+    const nome = req.body.nome
+    todoList[id] = nome
+    return res.json(todoList)
+})
+
+// Excluir algum curso
+app.delete('/:id', (req, res) => {
+    const id = req.params
+    todoList.splice(id, 1)
+    return res.json({
+        message: 'A tarefa foi deletada.'
+    })
+
 })
 
 app.listen(3000, () => {

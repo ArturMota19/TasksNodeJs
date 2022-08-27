b34edfb232a569e2bea8e738c4671d0bad58eb51
Artur.Mota <arturrodriguesmota@hotmail.com>
2022-08-27 11:16:40 -0300

    Finalização do projeto pra Sequelize e usando arquitetura MVC

--- 2_ Task_Sprint_Sequelize/index.js
+++ 2_ Task_Sprint_Sequelize/index.js
@@ -11,103 +11,11 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static('public'))
 app.use(express.static('public/images'));
-app.use('/tarefas', tarefasRoutes)
+app.use('/', tarefasRoutes)
 
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
-
-
-// POST PARA ADICIONAR TAREFA
-
-app.post('/addTarefa', async(req, res) => {
-    const nome = req.body.nome;
-    const prioridade = req.body.select;
-    var prioridadeAlta = 0
-    var prioridadeMedia = 0
-    var prioridadeBaixa = 0
-    if (prioridade == 'Alta') {
-        prioridadeAlta = 1
-    } else if (prioridade == 'Media') {
-        prioridadeMedia = 1
-    } else {
-        prioridadeBaixa = 1
-    }
-    await Tarefas.create({ nome, prioridade, prioridadeAlta, prioridadeMedia, prioridadeBaixa })
-    res.redirect('/')
-})
-
-// ROTA PRA ADICIONAR TAREFA
-app.get('/add', (req, res) => {
-    res.render('addtarefas')
-})
-
-// ROTA PRA PEGAR TODAS AS TAREFAS
-
-app.get('/', async(req, res) => {
-    const tarefas = await Tarefas.findAll({ raw: true })
-    res.render('tarefas', { tarefas: tarefas })
-})
-
-// ROTAS PRA PRIORIDADES
-
-app.get('/prioridadealta', async(req, res) => {
-    const tarefas = await Tarefas.findAll({ raw: true, where: { prioridade: 'Alta' } })
-    res.render('prioridadealta', { tarefas })
-})
-
-app.get('/prioridademedia', async(req, res) => {
-    const tarefas = await Tarefas.findAll({ raw: true, where: { prioridade: 'Media' } })
-    res.render('prioridademedia', { tarefas })
-})
-
-app.get('/prioridadebaixa', async(req, res) => {
-    const tarefas = await Tarefas.findAll({ raw: true, where: { prioridade: 'Baixa' } })
-    res.render('prioridadebaixa', { tarefas })
-})
-
-// ROTA PRA EDITAR TAREFA
-
-app.get('/edit/:id', async(req, res) => {
-    const id = req.params.id
-    const tarefas = await Tarefas.findOne({ raw: true, where: { id: id } })
-    res.render('editartarefa', { tarefas: tarefas })
-})
-
-app.post('/editartarefa', async(req, res) => {
-    const id = req.body.id
-    const nome = req.body.nome;
-    const prioridade = req.body.select;
-    var prioridadeAlta = 0
-    var prioridadeMedia = 0
-    var prioridadeBaixa = 0
-    if (prioridade == 'Alta') {
-        prioridadeAlta = 1
-    } else if (prioridade == 'Media') {
-        prioridadeMedia = 1
-    } else {
-        prioridadeBaixa = 1
-    }
-    const tarefaDados = {
-        id,
-        nome,
-        prioridade,
-        prioridadeAlta,
-        prioridadeMedia,
-        prioridadeBaixa
-    }
-    await Tarefas.update(tarefaDados, { where: { id: id } })
-    res.redirect('/')
-})
-
-
-// POST PARA REMOVER TAREFA
-app.post('/remove/:id', async(req, res) => {
-    const id = req.params.id
-    await Tarefas.destroy({ where: { id: id } })
-    res.redirect('/')
-})
-
-// Conexão
+    // Conexão
 
 conn.sync().then(() => {
     app.listen(3000)
--- 2_ Task_Sprint_Sequelize/routes/tarefasRoutes.js
+++ 2_ Task_Sprint_Sequelize/routes/tarefasRoutes.js
@@ -2,7 +2,16 @@ const express = require('express')
 const router = express.Router()
 const TarefasController = require('../controllers/TarefasController')
 
-router.get('/add', TarefasController.createTarefas)
-router.get('/', TarefasController.showTarefas)
+router.get('/', TarefasController.mostrarTarefas)
+router.get('/add', TarefasController.paginaCriarTarefas)
+router.post('/addTarefa', TarefasController.criarTarefas)
+router.get('/prioridadealta', TarefasController.prioridadeAlta)
+router.get('/prioridademedia', TarefasController.prioridadeMedia)
+router.get('/prioridadebaixa', TarefasController.prioridadeBaixa)
+router.get('/prioridadebaixa', TarefasController.prioridadeBaixa)
+router.get('/edit/:id', TarefasController.editarId)
+router.post('/editartarefa', TarefasController.paginaEditarId)
+router.post('/remove/:id', TarefasController.removerId)
+
 
 module.exports = router
